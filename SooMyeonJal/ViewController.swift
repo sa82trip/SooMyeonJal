88a9ac69b7f21a5203d3900e63066164c66dcff9
GJS <sa82trip@gmail.com>
2019-12-19 22:10:23 +0900

    #3 userdefaults setting -> 다음은 시간 계산해서 버튼 활성화 하는거

--- SooMyeonJal/ViewController.swift
+++ SooMyeonJal/ViewController.swift
@@ -10,21 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
+     
+    
+    @IBOutlet weak var morningButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        showsTime.text = UserDefaults.standard.string(forKey: "morning")
+        showsTimeBed.text = UserDefaults.standard.string(forKey: "night")
+        
     }
+    
+    
+    @IBOutlet weak var showsTime: UILabel!
+    
+    @IBOutlet weak var showsTimeBed: UILabel!
     
     @IBAction func stampMoring(_ sender: UIButton) {
         print("Good Morning")
         let now = makeCurrentTime()
         print(now)
+       
+       //누르면 버튼 비활성화 하기. 저녁 되면 풀린다.
+        morningButton.isUserInteractionEnabled=false
+        
+        //누른 시간 저장하기
+        UserDefaults.standard.set(now, forKey: "morning")
+        
     }
     
     @IBAction func stampNight(_ sender: UIButton) {
         print("Good night")
         let now  = makeCurrentTime()
         print(now)
+        
+        UserDefaults.standard.set(now, forKey: "night")
     }
     
     func makeCurrentTime()-> String{
@@ -32,9 +52,17 @@ class ViewController: UIViewController {
         let formatter = DateFormatter()
         formatter.dateStyle = .long
         formatter.timeStyle = .medium
-        
         return formatter.string(from: currentTime)
     }
     
+    
+    @IBAction func clearTime(_ sender: Any) {
+        showsTimeBed.text = ""
+        showsTime.text = ""
+        UserDefaults.standard.set(nil, forKey: "morning")
+        UserDefaults.standard.set(nil, forKey: "night")
+    }
 }
+
+
 
