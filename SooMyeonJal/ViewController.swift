22e6fd57d7bfd8f28b2617eafd3711ccf54babf8
GJS <sa82trip@gmail.com>
2019-12-21 10:08:03 +0900

    #5 confirm modal(alert modal)

--- SooMyeonJal/ViewController.swift
+++ SooMyeonJal/ViewController.swift
@@ -16,16 +16,26 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        var wokeupTime: Date = Date()
+        var goToBedTime: Date = Date()
+        if let morning = UserDefaults.standard.object(forKey: "wokeupTime") as? Date{
+            showsTime.text = stringifyTime(morning)
+            wokeupTime = morning
+            
+        }
+        if let night = UserDefaults.standard.object(forKey: "goToBedTime") as? Date{
+            showsTimeBed.text = stringifyTime(night)
+            goToBedTime = night
+        }
         
-        let morning = UserDefaults.standard.object(forKey: "morning") as! Date
-        let night = UserDefaults.standard.object(forKey: "night") as! Date
-        showsTime.text = morning.description(with: Locale.current)
-        showsTimeBed.text = night.description(with: Locale.current)
         
-        if(morning < night){
+        
+        if(wokeupTime < goToBedTime){
             print("night")
+            print(goToBedTime.description(with: Locale.current))
         }else {
             print("morning")
+            print(wokeupTime.description(with: Locale.current))
         }
         
         
@@ -38,26 +48,24 @@ class ViewController: UIViewController {
     
     @IBAction func stampMoring(_ sender: UIButton) {
         print("Good Morning")
-        let now = makeCurrentTime()
-        print(now)
+        
         let testDate = Date()
         print(testDate.description(with: Locale.current))
         //누르면 버튼 비활성화 하기. 저녁 되면 풀린다.
         morningButton.isUserInteractionEnabled=false
         
         //누른 시간 저장하기
-        UserDefaults.standard.set(testDate, forKey: "morning")
+        UserDefaults.standard.set(testDate, forKey: "wokeupTime")
         
         
     }
     
     @IBAction func stampNight(_ sender: UIButton) {
         print("Good night")
-        let now  = makeCurrentTime()
-        print(now)
+        
         let testDate = Date()
         print(testDate.description(with: Locale.current))
-        UserDefaults.standard.set(testDate, forKey: "night")
+        UserDefaults.standard.set(testDate, forKey: "goToBedTime")
         
     }
     
@@ -69,14 +77,36 @@ class ViewController: UIViewController {
         return formatter.string(from: currentTime)
     }
     
+    func stringifyTime(_ a: Date)->String{
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .medium
+        return formatter.string(from: a)
+    }
+    
     
     @IBAction func clearTime(_ sender: Any) {
-        showsTimeBed.text = ""
-        showsTime.text = ""
-        UserDefaults.standard.set(nil, forKey: "morning")
-        UserDefaults.standard.set(nil, forKey: "night")
+        
+        let alertController = UIAlertController(title: "Remove stored time", message: "want to delete?", preferredStyle: .alert)
+        let removeConfirm = UIAlertAction(title: "delete", style: .destructive, handler: { _ in
+            self.showsTimeBed.text = ""
+            self.showsTime.text = ""
+            UserDefaults.standard.set(nil, forKey: "wokeupTime")
+            UserDefaults.standard.set(nil, forKey: "goToBedTime")
+            return
+        })
+        
+        let cancelIt = UIAlertAction(title: "cancel", style: .cancel, handler: { (action)->Void in
+            print("cancel")
+        })
+        alertController.addAction(removeConfirm)
+        alertController.addAction(cancelIt)
+        present(alertController, animated: true, completion: nil)
+        
+        
     }
 }
 
 
 
+    
